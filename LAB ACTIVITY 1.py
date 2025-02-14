932c4f5ae7958a41801b3bb1b53943db6ee9ffd4
kkwwiinnii <quenniefayesanpedro@gmail.com>
2025-02-14 15:35:25 +0800

    Add files via upload

---
+++ LAB ACTIVITY 1.py
@@ -0,0 +1,50 @@
+DATA = {
+         "ACCELARATION": "The rate at which an object’s velocity changes over time. It includes both speed and direction.",
+         "ACCURACY": "The measurement of closeness to a true or standard value.",
+         "ARTIFICIAL INTELLIGENCE": "The demonstration of cognition by computers and machines.",
+         "BRITTLENESS": "A mechanical property of an object that measures the degree to which a material will fracture without significant deformation when subjected to stress.",
+         "COMPUTER-AIDED DESIGN": "The digital creation of two-dimensional drawings and three-dimensional models using a computer system. This is used to easily create, modify, analyze, share and optimize a design, which enables faster iteration and innovation.",
+         "COMPUTER-AIDED MANUFACTURING": "A manufacturing method in which software and computer-controlled machines execute high-precision tasks to build products. CAM has enabled the automation of processes that require a high degree of accuracy, giving manufacturers better quality control.",
+         "COMPUTER NUMERICAL CONTROL": "A computer-controlled manufacturing process in which programming dictates tasks to a wide range of factory tools and machinery, from drills and grinders to lathes. It can be used for three-dimensional cutting projects, such as the formation of metal tools from a block of material.",
+         "CODE COMPLIANCE": "The adherence to standardized area codes, from design standards to specifications and ordinances.",
+         "DESIGN FOR MANUFACTURABILITY": "A method of design optimization to facilitate straightforward and easy manufacturing with the goal of improving efficiencies so that costs are reduced but quality is not.",
+         "ELECTRICAL ENGINEERING": "The study, design, and practical application of devices and systems that use electricity and electronics. This branch of engineering includes everything from circuitry and power generation to communications systems.",
+         "ENGINEERING": "The application of scientific, mathematical, technological, economic, social, and practical knowledge to design and build structures, machines, devices, systems, materials, and processes.",
+         "ENGINEERING ETHICS": "Guiding ethical principles in the field of engineering that govern how engineers interact with society, the environment, their clients, and the world.",
+         "FEASIBILITY STUDIES": "An analysis used to evaluate projects, including the identification of best methods and techniques, infrastructure needs, potential pitfalls, and environmental considerations.",
+         "HEAT TRANSFER": "A discipline of thermal engineering involving the generation, use, and exchange of thermal energy between material systems.",
+         "INDUSTRIAL ENGINEERING": "An engineering sub-field dedicated to the optimization of complex processes and systems, particularly in the manufacturing industry. This includes the management of industrial resources such as labor, materials, and equipment to create efficient processes.",
+         "INSTRUMENTATION": "Tools that measure physical quantities or dimensions, ranging from thermometers to devices with complex multi-sensor components.",
+         "MACHINE LEARNING": "The development of computer systems using algorithms, patterns and statistical models so that they begin to learn and improve automatically without human intervention.",
+        "MATERIALS ENGINEERING": "The design and discovery of new and existing materials through the study of properties, such as atom consistency and behavior, using math, physics and chemistry.",
+        "MECHATRONICS": "A branch of engineering at the crossroads of mechanical, electronic, and computing that helps to improve efficiencies in manufacturing through technology and automation.",
+        "NANOTECHNOLOGY": "The manufacture and manipulation of matter on an atomic and molecular scale to produce innovative materials and devices.",
+        "PRECISION": "A characteristic of measurement that indicates the closeness of two or more measurements to each other, regardless of whether or not they are accurate. This defines the repeatability of a result under unchanged conditions.",
+        "QUALITY ASSURANCE": "The use of analysis and audit to ensure a company is meeting industry standards in manufacturing processes to help an entity prevent errors, defects, and problems.",
+        "QUALITY CONTROL": "An active process of monitoring and detection that helps to identify defects before a manufactured good is sent out to end users, thereby verifying that deliverables have met quality standards.",
+        "REPEATABILITY": "A reliability measurement that describes how well a system or part can repeatedly produce the same outcome in unchanged conditions.",
+        "ROBOTICS": "The design, construction, operation, and use of programmable machines known as robots that can often mimic human actions and completely or semi-autonomously perform physical tasks.",
+        "SEMICONDUCTOR": "A solid substance or material that has the ability to conduct electrical current between that of highly conductive materials, such as copper, and an insulator, such as glass.",
+        "SOLID MODELING": "The three-dimensional modeling of solids as aided by computer programming and mathematics to form a precise design through the application of geometric and topological measurements.",
+        "STATISTICS": "The branch of mechanics and physics that measures the forces acting on bodies at rest. The analysis of forces and torque on objects in static equilibrium with their surrounding environment.",
+        "TECHNICAL DRAWING": "A precise and detailed drawing that visually communicates how an object functions or should be constructed.",
+        "THERMODYNAMICS": "A branch of physics that studies the relationship between heat, temperature, and mechanical energy or work, including the conversion of one into the other, to understand the operation of a physical system."
+         
+
+}
+
+print("ENGINEERING DICTIONARY")
+print("NOTE: Type in all caps, avoid entering acronyms, and be mindful of hyphens. If error, term is not found or available.")
+print( )
+a = input("Search: ")
+print( )
+print("Definition")
+print(DATA[a])
+print( )
+print("REFERENCE: The Engineer's Glossary. n.d. 3DEO Metal 3d Printing. https://www.3deo.co/featured/engineering-terms-the-engineers-glossary-for-3deo/")
+
+
+
+
+
+
